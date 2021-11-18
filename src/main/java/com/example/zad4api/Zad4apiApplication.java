e05654f13d11e288afd312d4c0884492d2e5bc89
klesniewski50 <g3zv1ag8dx1ms2@gmail.com>
2021-11-18 12:53:30 +0100

    stworzenie projektu

---
+++ src/main/java/com/example/zad4api/Zad4apiApplication.java
@@ -0,0 +1,13 @@
+package com.example.zad4api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zad4apiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zad4apiApplication.class, args);
+    }
+
+}
